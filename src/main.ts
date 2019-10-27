78cc017e3c0bf137e99c670bbec27542a481fc14
chris sidi <chrisid@microsoft.com>
2019-10-26 22:54:22 -0400

    Add PullRequestTracker class
    Take an array of args

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,8 @@ import * as azdev from "azure-devops-node-api";
 import * as ga from "azure-devops-node-api/GitApi";
 import * as ra from "azure-devops-node-api/ReleaseApi";
 import { PullRequestStatus } from "azure-devops-node-api/interfaces/GitInterfaces";
-import { config } from "dotenv";
 import { ReleaseEnvironment, ReleaseStatus, ReleaseQueryOrder, Release, ReleaseExpands, EnvironmentStatus, ReleaseDefinitionEnvironment } from "azure-devops-node-api/interfaces/ReleaseInterfaces";
-config();
+import { IRequestHandler } from "azure-devops-node-api/interfaces/common/VsoBaseInterfaces";
 
 export interface pullRequestInfo {
   id: number,
@@ -31,144 +30,195 @@ export interface pullRequestDeployInfo {
   deployedEnvironments?: environmentDictionary
 }
 
-// your collection url
-const orgUrl = "https://dev.azure.com/mseng";
-const project = "AzureDevOps";
-const releaseDefinitionId = 3358;
+class PullRequestTracker {
+  project: string;
+  releaseDefinitionId: number;
+  authHandler: IRequestHandler;
+  connection: azdev.WebApi;
 
-if (!process.env.AZURE_PERSONAL_ACCESS_TOKEN) {
-  throw "AZURE_PERSONAL_ACCESS_TOKEN is undefined.";
-}
-let token: string = process.env.AZURE_PERSONAL_ACCESS_TOKEN;
-
-let authHandler = azdev.getPersonalAccessTokenHandler(token);
-let connection = new azdev.WebApi(orgUrl, authHandler);
-
-async function getDeployInfo(pullRequestId: number) {
-  let git: ga.IGitApi = await connection.getGitApi();
-  let rm: ra.IReleaseApi = await connection.getReleaseApi();
-
-  const pullRequest = await git.getPullRequestById(pullRequestId);
-  const prCommitId = pullRequest.lastMergeCommit!.commitId!;
-  // console.log(pullRequest);
-  // console.log();
-  // console.log(pullRequest.status);
-
-  let progressInfo: pullRequestDeployInfo = {
-    pullRequest: {
-      id: pullRequestId,
-      isDraft: pullRequest.isDraft!,
-      status: pullRequest.status!
-    },
-    deployedToAllEnvironments: false
-  };
-
-  if (pullRequest.status != PullRequestStatus.Completed) {
-    return progressInfo;
+  constructor(orgUrl: string, project: string, personalAccessToken: string, releaseDefinitionId: number) {
+    this.project = project;
+    this.releaseDefinitionId = releaseDefinitionId;
+    this.authHandler = azdev.getPersonalAccessTokenHandler(personalAccessToken);
+    this.connection = new azdev.WebApi(orgUrl, this.authHandler);
   }
 
-  progressInfo.pullRequest.completionTime = pullRequest.completionQueueTime;
-  progressInfo.pullRequest.commitId = prCommitId;
+  async getDeployInfos(pullRequestIds: number[]) {
+    const git: ga.IGitApi = await this.connection.getGitApi();
+    const rm: ra.IReleaseApi = await this.connection.getReleaseApi();
 
-  const environments = await getEnvironments(rm);
-  const minCreationTime = progressInfo.pullRequest.completionTime!;
-  const top = 1000;
-  const releases = await rm.getReleases(project, releaseDefinitionId, undefined, undefined, undefined, ReleaseStatus.Active, undefined, minCreationTime, undefined, ReleaseQueryOrder.Ascending, top, undefined, ReleaseExpands.Artifacts | ReleaseExpands.Environments);
+    const pullRequestInfos = await this.getPullRequestInfos(git, pullRequestIds);
 
-  progressInfo.deployedEnvironments = {};
-  for (const release of releases) {
-    if (!release.environments) {
-      throw "Release doesn't include environments"
+    let deployInfos = pullRequestInfos.map(pullRequest => <pullRequestDeployInfo>{
+      pullRequest,
+      deployedToAllEnvironments: false,
+    });
+
+    const completedPRs = deployInfos.filter(info => info.pullRequest.status === PullRequestStatus.Completed);
+    if (!completedPRs.length) {
+      return deployInfos;
     }
 
-    const succeededEnvironments = release.environments.filter(environmentFilters);
-    if (!succeededEnvironments.length) {
-      continue;
-    }
+    const environments = await this.getEnvironments(rm);
+    const minCreationTime = this.min(completedPRs.map(info => info.pullRequest.completionTime!))!;
+    const top = 1000;
+    const releases = await rm.getReleases(this.project, this.releaseDefinitionId, undefined, undefined, undefined, ReleaseStatus.Active, undefined, minCreationTime, undefined, ReleaseQueryOrder.Ascending, top, undefined, ReleaseExpands.Artifacts | ReleaseExpands.Environments);
 
-    if (!await releaseBuildIncludesCommit(git, release, prCommitId)) {
-      console.log(`Release ${release.name} doesn't include PR !${pullRequestId}`);
-      continue;
-    }
+    for (const progressInfo of completedPRs) {
+      progressInfo.deployedEnvironments = {};
+      for (const release of releases) {
+        if (progressInfo.deployedToAllEnvironments) {
+          break;
+        }
 
-    for (const environment of succeededEnvironments) {
-      // console.log(`Release ${release.name}, environment ${environment.name}`);
+        if (!release.environments) {
+          throw "Release doesn't include environments"
+        }
 
-      if (!progressInfo.deployedEnvironments[environment.name!]) {
-        progressInfo.deployedEnvironments[environment.name!] = {
-          name: environment.name!,
-          firstReleaseName: release.name!
-        };
+        const succeededEnvironments = release.environments.filter(e => this.environmentFilters(e));
+        if (!succeededEnvironments.length) {
+          continue;
+        }
 
-        if (Object.keys(progressInfo.deployedEnvironments).length === environments.length) {
-          progressInfo.deployedToAllEnvironments = true;
-          return progressInfo;
+        // If the release was created before the PR was merged, we know it can't contain
+        // the PR's changes. We don't need to make the server call below.
+        if (release.createdOn! < progressInfo.pullRequest.completionTime!) {
+          continue;
+        }
+
+        if (!await this.releaseBuildIncludesCommit(git, release, progressInfo.pullRequest.commitId!)) {
+          // console.log(`Release ${release.name} doesn't include PR !${progressInfo.pullRequest.id}`);
+          continue;
+        }
+
+        for (const environment of succeededEnvironments) {
+          // console.log(`Release ${release.name}, environment ${environment.name}`);
+
+          if (!progressInfo.deployedEnvironments[environment.name!]) {
+            progressInfo.deployedEnvironments[environment.name!] = {
+              name: environment.name!,
+              firstReleaseName: release.name!
+            };
+
+            if (Object.keys(progressInfo.deployedEnvironments).length === environments.length) {
+              progressInfo.deployedToAllEnvironments = true;
+            }
+          }
         }
       }
     }
+
+    return deployInfos;
   }
 
-  return progressInfo;
-}
+  private async getPullRequestInfos(git: ga.IGitApi, pullRequestIds: number[]) {
+    let pullRequestInfos: pullRequestInfo[] = [];
 
-async function getEnvironments(rm: ra.IReleaseApi) {
-  const definition = await rm.getReleaseDefinition(project, releaseDefinitionId);
-  if (!definition.environments) {
-    throw "Release definition doesn't include environments"
-  }
-  return definition.environments.filter(environmentNameFilter);
-}
+    for (const pullRequestId of pullRequestIds) {
+      const pullRequest = await git.getPullRequestById(pullRequestId);
+      const prCommitId = pullRequest.lastMergeCommit!.commitId!;
+      // console.log(pullRequest);
+      // console.log();
+      // console.log(pullRequest.status);
 
-function environmentNameFilter(environment: ReleaseEnvironment | ReleaseDefinitionEnvironment) {
-  return environment.name !== "Single Scale Unit";
-  // const result = environment.name != "Single Scale Unit";
-  // console.log(`${environment.name}: ${result}`);
-  // return result;
-}
+      const pullRequestInfo: pullRequestInfo = {
+        id: pullRequestId,
+        isDraft: pullRequest.isDraft!,
+        status: pullRequest.status!
+      };
 
-function environmentStatusFilter(environment: ReleaseEnvironment) {
-  return environment.status === EnvironmentStatus.Succeeded
-      || environment.status === EnvironmentStatus.PartiallySucceeded;
-}
+      if (pullRequest.status == PullRequestStatus.Completed) {
+        pullRequestInfo.completionTime = pullRequest.completionQueueTime;
+        pullRequestInfo.commitId = prCommitId;
+      }
 
-function environmentFilters(environment: ReleaseEnvironment) {
-  return environmentNameFilter(environment) && environmentStatusFilter(environment);
-}
+      pullRequestInfos.push(pullRequestInfo);
+    }
 
-async function releaseBuildIncludesCommit(git: ga.GitApi, release: Release, commitId: string) {
-  if (!release.artifacts) {
-    console.log("Release doesn't include artifacts");
-    throw "Release doesn't include artifacts.";
-  }
-  const build = release.artifacts![0].definitionReference!;
-  const buildCommitId = build.sourceVersion.id!;
-  const repositoryId = build.repository.id!;
-
-  if (buildCommitId === commitId) {
-    return true;
+    return pullRequestInfos;
   }
 
-  // console.log(`Checking merge base for ${buildCommitId} and ${commitId}...`);
-  const mergeBases = await git.getMergeBases(repositoryId, buildCommitId, commitId, project);
+  private async getEnvironments(rm: ra.IReleaseApi) {
+    const definition = await rm.getReleaseDefinition(this.project, this.releaseDefinitionId);
+    if (!definition.environments) {
+      throw "Release definition doesn't include environments"
+    }
+    return definition.environments.filter(this.environmentNameFilter);
+  }
 
-  for (const mergeBase of mergeBases) {
-    if (mergeBase.commitId === commitId) {
+  private environmentNameFilter(environment: ReleaseEnvironment | ReleaseDefinitionEnvironment) {
+    return environment.name !== "Single Scale Unit";
+    // const result = environment.name != "Single Scale Unit";
+    // console.log(`${environment.name}: ${result}`);
+    // return result;
+  }
+
+  private environmentStatusFilter(environment: ReleaseEnvironment) {
+    return environment.status === EnvironmentStatus.Succeeded
+        || environment.status === EnvironmentStatus.PartiallySucceeded;
+  }
+
+  private environmentFilters(environment: ReleaseEnvironment) {
+    return this.environmentNameFilter(environment) && this.environmentStatusFilter(environment);
+  }
+
+  private async releaseBuildIncludesCommit(git: ga.GitApi, release: Release, commitId: string) {
+    if (!release.artifacts) {
+      console.log("Release doesn't include artifacts");
+      throw "Release doesn't include artifacts.";
+    }
+    const build = release.artifacts![0].definitionReference!;
+    const buildCommitId = build.sourceVersion.id!;
+    const repositoryId = build.repository.id!;
+
+    if (buildCommitId === commitId) {
       return true;
     }
+
+    // console.log(`Checking merge base for ${buildCommitId} and ${commitId}...`);
+    const mergeBases = await git.getMergeBases(repositoryId, buildCommitId, commitId, this.project);
+
+    for (const mergeBase of mergeBases) {
+      if (mergeBase.commitId === commitId) {
+        return true;
+      }
+    }
+    return false;
   }
-  return false;
+
+  private min<T>(values: T[]) {
+    let minValue: T | undefined = undefined;
+    for (const value of values) {
+      if (minValue === undefined || minValue > value) {
+        minValue = value;
+      }
+    }
+
+    return minValue;
+  }
 }
 
 (async function main() {
-  console.log(await getDeployInfo(494919));
+  // your collection url
+  const orgUrl = "https://dev.azure.com/mseng";
+  const project = "AzureDevOps";
+  const releaseDefinitionId = 3358;
+
+  require("dotenv").config();
+
+  if (!process.env.AZURE_PERSONAL_ACCESS_TOKEN) {
+    throw "AZURE_PERSONAL_ACCESS_TOKEN is undefined.";
+  }
+  let token: string = process.env.AZURE_PERSONAL_ACCESS_TOKEN;
+
+  const tracker = new PullRequestTracker(orgUrl, project, process.env.AZURE_PERSONAL_ACCESS_TOKEN, releaseDefinitionId);
+
+  console.log(await tracker.getDeployInfos([494919]));
   console.log();
 
-  console.log(await getDeployInfo(510903));
+  console.log(await tracker.getDeployInfos([510903]));
   console.log();
 
-  console.log(await getDeployInfo(513131));
-  console.log();
-
-  console.log(await getDeployInfo(513094));
+  console.log(await tracker.getDeployInfos([513131, 513094]));
   console.log();
 })();
